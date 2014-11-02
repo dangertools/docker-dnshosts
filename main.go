90a1c64692497247b37d7535e424f2fa75b875b4
Andrew Dunham <andrew@du.nham.ca>
2014-11-02 15:18:57 -0800

    go fmt

--- main.go
+++ main.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-    "os"
-    "log"
-    
-    flags "github.com/jessevdk/go-flags"
-    dockerapi "github.com/fsouza/go-dockerclient"
+	"log"
+	"os"
+
+	dockerapi "github.com/fsouza/go-dockerclient"
+	flags "github.com/jessevdk/go-flags"
 )
 
 func getopt(name, def string) string {
-    if env := os.Getenv(name); env != "" {
-        return env
-    }
-    
-    return def
+	if env := os.Getenv(name); env != "" {
+		return env
+	}
+
+	return def
 }
 
 func assert(err error) {
-    if err != nil {
-        log.Fatal("docker-hosts: ", err)
-    }
+	if err != nil {
+		log.Fatal("docker-hosts: ", err)
+	}
 }
 
 type Options struct {
 	DomainName string `short:"d" long:"domain-name" description:"domain to append"`
-	File struct {
+	File       struct {
 		Filename string
 	} `positional-args:"true" required:"true" description:"the hosts file to write"`
 }
 
 func main() {
 	var opts Options
-	
+
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		os.Exit(1)
 	}
-	
-    docker, err := dockerapi.NewClient(getopt("DOCKER_HOST", "unix:///var/run/docker.sock"))
-    assert(err)
 
-    hosts := NewHosts(docker, opts.File.Filename, opts.DomainName)
+	docker, err := dockerapi.NewClient(getopt("DOCKER_HOST", "unix:///var/run/docker.sock"))
+	assert(err)
 
-    // set up to handle events early, so we don't miss anything while doing the
-    // initial population
-    events := make(chan *dockerapi.APIEvents)
-    assert(docker.AddEventListener(events))
+	hosts := NewHosts(docker, opts.File.Filename, opts.DomainName)
 
-    containers, err := docker.ListContainers(dockerapi.ListContainersOptions{})
-    assert(err)
+	// set up to handle events early, so we don't miss anything while doing the
+	// initial population
+	events := make(chan *dockerapi.APIEvents)
+	assert(docker.AddEventListener(events))
 
-    for _, listing := range containers {
-        go hosts.Add(listing.ID)
-    }
+	containers, err := docker.ListContainers(dockerapi.ListContainersOptions{})
+	assert(err)
 
-    log.Println("docker-hosts: Listening for Docker events...")
-    for msg := range events {
-        switch msg.Status {
-        case "start":
-            go hosts.Add(msg.ID)
+	for _, listing := range containers {
+		go hosts.Add(listing.ID)
+	}
 
-        case "die":
-            go hosts.Remove(msg.ID)
-        }
-    }
+	log.Println("docker-hosts: Listening for Docker events...")
+	for msg := range events {
+		switch msg.Status {
+		case "start":
+			go hosts.Add(msg.ID)
 
-    log.Fatal("docker-hosts: docker event loop closed") // todo: reconnect?
+		case "die":
+			go hosts.Remove(msg.ID)
+		}
+	}
+
+	log.Fatal("docker-hosts: docker event loop closed") // todo: reconnect?
 }
